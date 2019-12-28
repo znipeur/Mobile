d5f705cb7322352773bc205be74791ecde50fdef
oussama <oussamasebbani@hotmail.fr>
2019-12-28 22:30:34 +0100

    map

--- app/src/main/java/com/example/projetparking/HomeActivity.java
+++ app/src/main/java/com/example/projetparking/HomeActivity.java
@@ -3,6 +3,7 @@ package com.example.projetparking;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,9 +11,12 @@ import android.widget.Button;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.net.URI;
+
 public class HomeActivity extends AppCompatActivity {
     Button btnLogout;
     FirebaseAuth mFirebaseAuth;
+    Button btnChemin;
     private FirebaseAuth.AuthStateListener mAuthStateListener;
 
     @Override
@@ -20,6 +24,7 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         btnLogout = findViewById(R.id.logout);
+        btnChemin = findViewById(R.id.chemin);
 
         btnLogout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,6 +32,15 @@ public class HomeActivity extends AppCompatActivity {
                 FirebaseAuth.getInstance().signOut();
                 Intent intToMain = new Intent(HomeActivity.this,MainActivity.class);
                 startActivity(intToMain);
+            }
+        });
+        btnChemin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri gmmIntentUri = Uri.parse("google.navigation:q=82 Avenue des Nations Unies+ Rabat");
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW,gmmIntentUri);
+                mapIntent.setPackage("com.google.android.apps.maps");
+                startActivity(mapIntent);
             }
         });
     }
